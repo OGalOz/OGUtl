c24ea26376ae7235908f510d8efbddef7892c0a4
Omree Gal-Oz <ogaloz@lbl.gov>
2022-01-27 13:37:25 -0800

    Trie Update

--- CreateTrieJSON.py
+++ CreateTrieJSON.py
@@ -7,6 +7,7 @@ The idea is to store it in as small a way as possible.
 
 import sys, os, logging, json
 from typing import List, Dict
+from OGUtil import OGUtil 
 
 def initialize_Trie():
     '''
@@ -53,10 +54,35 @@ def get_all_words_in_Trie(T, crt: str) -> List[str]:
     for c in T["c"]:
         word_list += get_all_words_in_Trie(T["c"][c], crt + c)
     return word_list
-    
+
+def get_prefix_Trie(T, prefix):
+    if len(prefix) == 0:
+        return T
+    if prefix[0] in T["c"]:
+        return get_prefix_Trie(T["c"][prefix[0]], prefix[1:])
+    else:
+        return None
 
 
+def get_all_words_that_start_with(T, prefix: str) -> List[str]:
+    pT: Dict = get_prefix_Trie(T, prefix)
+    if pT is not None:
+        word_list = get_all_words_in_Trie(pT, prefix)
+        return word_list
+    else:
+        return []
 
+
+def export_Trie(T, op_fp: str) -> None:
+    with open(op_fp, "w") as g:
+        g.write(json.dumps(T))
+
+# Given JSON file with list of words to make Trie, do
+def JSON_word_list_to_JSON_Trie(inp_json_fp: str, op_json_fp) -> None:
+    O = OGUtil()
+    word_list: List[str] = O.load_json(inp_json_fp)
+    T: Dict = add_list_of_words_to_new_trie(word_list)
+    O.export_json(op_json_fp, T, dbg=True)
 
 
 def add_list_of_words_to_new_trie(L: List[str]) -> bool:
@@ -67,15 +93,21 @@ def add_list_of_words_to_new_trie(L: List[str]) -> bool:
     return T
 
 def quick_test():
-    L: List[str] = ["apple", "app", "banana", "Appalachia"]
+    L: List[str] = ["apple", "app", "banana", "Appalachia", "band", "bandana", 
+                    "bag"]
     T: Dict = add_list_of_words_to_new_trie(L)
-    print(T)
-    print(check_if_word_in_Trie(T, "apple"))
-    print(check_if_word_in_Trie(T, "APPalachia"))
-    print(get_all_words_in_Trie(T, ""))
+
+    print(get_all_words_that_start_with(T, "ban"))
+    print(get_all_words_that_start_with(T, "ba"))
+    print(get_all_words_that_start_with(T, "bag"))
+
+
+def import_export_test(inp_fp, op_fp):
+    JSON_word_list_to_JSON_Trie(inp_fp, op_fp)
 
 def main():
-    quick_test()
+    _, inp_fp, op_fp = sys.argv
+    import_export_test(inp_fp, op_fp)
 
 
 if __name__ == "__main__":
--- OGUtil.py
+++ OGUtil.py
@@ -1,9 +1,26 @@
 import sys
-
+import json
+import logging
+import os
+from typing import List, Dict
 
 class OGUtil:
-    def __init__(self, logger):
-        self.l = logger
+    def __init__(self, logger=None):
+        if logger is not None:
+            self.l = logger
     def ex(self, inp):
         raise Exception(str(inp))
+    def load_json(self, fp):
+        with open(fp, 'r') as f:
+            x = json.loads(f.read())
+        return x
+    def export_json(self, fp: str, obj, indent: int = 0,
+                    dbg: bool = False) -> None:
+        with open(fp, 'w') as g:
+            if indent == 0:
+                g.write(json.dumps(obj))
+            else:
+                g.write(json.dumps(obj, indent=abs(indent)))
+        if dbg:
+            print("Wrote out JSON object to " + fp)
 
